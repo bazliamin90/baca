e23b3062d28d4cc444111537f54fa050f3858e46
bazliamin90 <bez.aminuddin@gmail.com>
2025-05-01 15:43:59 +0800

    add

--- header.js
+++ header.js
@@ -175,7 +175,7 @@ ol li {
     </li>
     <li><a href="002-ntm.html">Nun, tanwin, mim</a></li>
     <li><a href="004-sakinain.html">Pertemuan dua sukun</a></li>
-    <li><a href="index.html">Waqaf Jibril</a></li>
+    <li><a href="001-wj.html">Waqaf Jibril</a></li>
   <hr><b>Nota tambahan</b><hr class="hrnone">
     <li><a href="https://bazliamin90.github.io/bukutajwid" target="_blank">Buku tajwid</a></li>
   </ol>
